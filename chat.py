d2a8f1ceed52fe5a09ddc1d8f965a75727b1e73a
Josh Fernando <112506062+JoshFernando@users.noreply.github.com>
2023-08-20 00:47:18 -0400

    creating the chatbot, diagflow (google cloud products)

---
+++ chat.py
@@ -0,0 +1,90 @@
+import random
+import json
+
+
+import torch
+import dialogflow_v2 as dialogflow
+
+from google.protobuf.json_format import MessageToJson
+
+from model import NeuralNet
+from nltk_utils import bag_of_words, tokenize
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+with open('intents.json', 'r') as json_data:
+    intents = json.load(json_data)
+
+FILE = "data.pth"
+data = torch.load(FILE)
+
+input_size = data["input_size"]
+hidden_size = data["hidden_size"]
+output_size = data["output_size"]
+all_words = data['all_words']
+tags = data['tags']
+model_state = data["model_state"]
+
+model = NeuralNet(input_size, hidden_size, output_size).to(device)
+model.load_state_dict(model_state)
+model.eval()
+
+bot_name = "Sam"
+print("Let's chat! (type 'quit' to exit)")
+while True:
+    # sentence = "do you use credit cards?"
+    sentence = input("You: ")
+    if sentence == "quit":
+        break
+
+    sentence = tokenize(sentence)
+    X = bag_of_words(sentence, all_words)
+    X = X.reshape(1, X.shape[0])
+    X = torch.from_numpy(X).to(device)
+
+    output = model(X)
+    _, predicted = torch.max(output, dim=1)
+
+    tag = tags[predicted.item()]
+
+    probs = torch.softmax(output, dim=1)
+    prob = probs[0][predicted.item()]
+    if prob.item() > 0.75:
+        for intent in intents['intents']:
+            if tag == intent["tag"]:
+                print(f"{bot_name}: {random.choice(intent['responses'])}")
+    else:
+        print(f"{bot_name}: I do not understand...")
+
+        import os
+
+def detect_intent(project_id, session_id, text, language_code='en'):
+    session_client = dialogflow.SessionsClient()
+
+    session = session_client.session_path(project_id, session_id)
+
+    text_input = dialogflow.TextInput(text=text, language_code=language_code)
+    query_input = dialogflow.QueryInput(text=text_input)
+
+    response = session_client.detect_intent(
+        request={"session": session, "query_input": query_input}
+    )
+
+    return response.query_result
+
+def main():
+    project_id = "your-project-id"  # Replace with your actual Dialogflow project ID
+    session_id = "unique-session-id"  # Provide a unique session ID for each conversation
+
+    while True:
+        user_input = input("You: ")
+        if user_input.lower() == 'exit':
+            print("Chatbot: Goodbye!")
+            break
+
+        response = detect_intent(project_id, session_id, user_input)
+        bot_response = response.fulfillment_text
+        print("Chatbot:", bot_response)
+
+if __name__ == '__main__':
+    main()
